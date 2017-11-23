7ba76921ce72ab149338a822990984705031413a
ffahri <admin@webischia.com>
2017-11-23 23:58:36 +0300

    getting id with JPA

--- src/main/java/com/webischia/test1/repositories/RecipeRepository.java
+++ src/main/java/com/webischia/test1/repositories/RecipeRepository.java
@@ -3,6 +3,8 @@ package com.webischia.test1.repositories;
 import com.webischia.test1.domain.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface RecipeRepository extends CrudRepository<Recipe,Long> {
 
 }
